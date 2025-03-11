31ad0f764fdafbac11828df871fd5d2932891a0f
Your Name <koom@c1>
2025-03-11 14:57:42 +0100

    feat: Add initial notification page structure with Tauri event and core imports

---
+++ src/routes/notification/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+
+    import {onMount} from "svelte";
+
+    const {listen} = window.__TAURI__.event;
+    const {invoke} = window.__TAURI__.core;
+
+    let id = '';
+    const DEBUG = true;
+    let data;
+
+    onMount(async () => {
+
+
+
+    });
+
+</script>
