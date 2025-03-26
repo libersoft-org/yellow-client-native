a95d99edfaa0429fd3e5b25c332dad95d5838c4c
Jindrich Kolman <kolman.jindrich@gmail.com>
2025-03-26 16:35:41 +0100

    build

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ import { copySync } from 'fs-extra';
 import path from 'path';
 
 const clientPath = path.resolve(__dirname, '../yellow-client');
-const buildPath = path.join(clientPath, 'build');
+const buildPath = path.join(clientPath, 'build-tauri');
 const nativePath = path.resolve(__dirname, '../yellow-client-native');
 
 execSync('bun run build', {
@@ -12,4 +12,7 @@ execSync('bun run build', {
     stdio: 'inherit'
 });
 
-copySync(buildPath, nativePath + '/');
+// const targetPath = path.join(nativePath, 'build-tauri');
+// console.log(`Copying build from ${buildPath} to ${targetPath}`);
+// copySync(buildPath, targetPath);
+//
--- src-tauri/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd ../yellow-client; TAURI=true bun run build; cp -r build ../yellow-client-native
