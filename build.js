499dc9f49e6437239714e740bf1393dc5c569bee
Jindrich Kolman <kolman.jindrich@gmail.com>
2025-04-04 17:58:21 +0200

    ci..

--- build.js
+++ build.js
@@ -1,10 +1,7 @@
 import { execSync } from 'child_process';
-import { copySync } from 'fs-extra';
 import path from 'path';
 
 const clientPath = path.resolve(__dirname, '../yellow-client');
-const buildPath = path.join(clientPath, 'build-tauri');
-const nativePath = path.resolve(__dirname, '../yellow-client-native');
 
 execSync('bun run build', {
     cwd: clientPath,
